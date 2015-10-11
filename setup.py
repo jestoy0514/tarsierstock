af00a0ebd785fe6df70b8c313a0c7c9ff0f5836a
jestoy0514 <jestoy.olazo@gmail.com>
2015-10-11 16:02:17 +0400

    Change the name of the license.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=[''],
     url='http://jestoy0514.github.io/tarsierstock',
-    license='GNU Public License version 2',
+    license='GNU General Public License Version 2',
     author='Jesus Vedasto Olazo',
     author_email='jestoy.olazo@gmail.com',
     description='A simple stock management system.'
