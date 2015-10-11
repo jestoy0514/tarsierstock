4a8b1592be78688f11d62288d4d53e8818a1a54f
jestoy0514 <jestoy.olazo@gmail.com>
2015-10-11 12:55:13 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Tarsier Stock',
+    version='0.1',
+    packages=[''],
+    url='http://jestoy0514.github.io/tarsierstock',
+    license='GNU Public License version 2',
+    author='Jesus Vedasto Olazo',
+    author_email='jestoy.olazo@gmail.com',
+    description='A simple stock management system.'
+)
