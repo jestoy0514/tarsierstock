5f4dd3968f282f9843a3f3373443b602c6c4eb78
jestoy0514 <jestoy.olazo@gmail.com>
2023-10-01 00:06:14 +0400

    started to use pysimplegui, added menu button and menubar

--- tarsierstock.py
+++ tarsierstock.py
@@ -2,7 +2,7 @@
 #
 # tarsierstock.py - A simple inventory software.
 #
-# Copyright (c) 2015 - Jesus Vedasto Olazo <jessie@jestoy.frihost.net>
+# Copyright (c) 2023 - Jesus Vedasto Olazo <jestoy.olazo@gmail.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,7 +19,36 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301, USA.
 
-from application.application import main
+import PySimpleGUI as sg
 
-if __name__ == '__main__':
-    main()
+__appname__ = "Tarsier Stock"
+__description__ = "A simple inventory software for small business."
+__version__ = "1.0"
+__author__ = "Jesus Vedasto Olazo"
+__email__ = "jestoy.olazo@gmail.com"
+__web__ = "https://jesusolazo.rf.gd"
+__license__ = "GPLV2"
+__status__ = "Development"
+__maintainer__ = "Jesus Vedasto Olazo"
+__copyright__ = "Copyright (c) 2023 - Jesus Vedasto Olazo"
+
+# Set theme to use
+sg.theme('BluePurple')
+
+# Widgets layout
+layout = [
+    [sg.Menu([['File', ['Quit']], ['Option'], ['Help']])],
+    [sg.B('Product'), sg.B('Incoming'), sg.B('Outgoing'),
+    sg.B('Report'), sg.B('Quit')]
+    ]
+
+window = sg.Window(f'Tarsier Stock - {__version__}', layout)
+
+while True:
+    # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Quit':
+        break
+
+window.close()
